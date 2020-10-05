fc87b13ff02f18894671c35fb017366883d7a9e2
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-05 06:36:01 +0200

    Customer list and detail

--- firestarter/src/app/app-routing.module.ts
+++ firestarter/src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
     loadChildren: () =>
       import('./kanban/kanban.module').then(m => m.KanbanModule),
     canActivate: [AuthGuard]
-  },
+  }, {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then(m => m.CustomersModule),
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
